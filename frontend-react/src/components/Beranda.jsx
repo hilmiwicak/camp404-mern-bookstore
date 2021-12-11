b9537812d550453e859de3ab1dc0b6973beb2784
hilmiwicak <hilmiwicaksonop@gmail.com>
2021-12-11 19:07:24 +0700

    import beranda

---
+++ frontend-react/src/components/Beranda.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Beranda() {
+  // data
+  // event handling
+  return (
+    <div className="container mt-3 w-75">
+      <h1 className="text-center">Selamat Datang di toko buku camp404-NIC:C4211116010</h1>
+      <div id="katalog"></div>
+    </div>
+  );
+}
---
+++ frontend-react/src/components/ManajemenBuku.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function ManajemenBuku() {
+  // data
+  // event handling
+  return (
+    <div className="container mt-3">
+      <h1 className="text-center">Manajemen Buku</h1>
+      <div id="formTambah"></div>
+      <div id="formUbah"></div>
+      <div id="daftarBuku"></div>
+    </div>
+  );
+}
