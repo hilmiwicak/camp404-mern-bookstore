3b1b4b727249ebe09f1ff80b8e97c02b3095c04c
hilmiwicak <hilmiwicaksonop@gmail.com>
2021-12-12 19:29:02 +0700

    read book list

--- frontend-react/src/components/ManajemenBuku.jsx
+++ frontend-react/src/components/ManajemenBuku.jsx
@@ -9,7 +9,38 @@ export default class ManajemenBuku extends React.Component {
           <h1 className="text-center">Manajemen Buku</h1>
           <div id="formTambah"></div>
           <div id="formUbah"></div>
-          <div id="daftarBuku"></div>
+          <div id="daftarBuku">
+              <h2 className="mt-3">Daftar Buku</h2>
+              <hr />
+              <button className="btn btn-primary m-2">Tambah Buku</button>
+              <table className="table table-bordered">
+                  <thead>
+                      <tr>
+                          <th>No.</th>
+                          <th>Judul</th>
+                          <th>Pengarang</th>
+                          <th>Harga</th>
+                          <th>Stok</th>
+                          <th>Aksi</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {this.props.bookList.map((book, index) => (
+                          <tr key={index}>
+                              <td>{book._id}</td>
+                              <td>{book.judul}</td>
+                              <td>{book.pengarang}</td>
+                              <td>{book.harga}</td>
+                              <td>{book.stok}</td>
+                              <td>
+                                  <button className="btn btn-info mr-3">Edit</button>
+                                  <button className="btn btn-danger mr-3">Hapus</button>
+                              </td>
+                          </tr>
+                      ))}
+                  </tbody>
+              </table>
+          </div>
         </div>
       </Fragment>
     );
