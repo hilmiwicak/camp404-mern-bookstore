79fc00a6e1b51edf4824e4b592975ea0e5962e8e
hilmiwicak <hilmiwicaksonop@gmail.com>
2021-12-14 16:08:13 +0700

    fetch books from database

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Beranda from "./components/Beranda";
 import Navbar from "./components/Navbar";
@@ -8,27 +9,25 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: [
-        {
-          _id: 1,
-          judul: "Laskar Pelangi",
-          pengarang: "Andrea Hirata",
-          harga: 86906,
-          stok: 7,
-        },
-        {
-          _id: 2,
-          judul: "Bumi",
-          pengarang: "Tere Liye",
-          harga: 85009,
-          stok: 5,
-        },
-      ],
+      books: [],
     };
 
     this.storeData = this.storeData.bind(this);
     this.updateData = this.updateData.bind(this);
     this.deleteDa = this.deleteData.bind(this);
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:4000/book")
+      .then((response) => {
+        this.setState({
+          books: response.data,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   storeData(inputBook) {
@@ -47,32 +46,36 @@ export default class App extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route
-              exact
-              path="/"
-              element={<Beranda bookList={this.state.books} />}
-            />
+    if (this.state.books.length === 0) {
+      return <p className="text-center">Loading ...</p>;
+    } else {
+      return (
+        <div>
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={<Beranda bookList={this.state.books} />}
+              />
 
-            <Route
-              exact
-              path="/manajemen-buku"
-              element={
-                <ManajemenBuku
-                  bookList={this.state.books}
-                  store={this.storeData}
-                  update={this.updateData}
-                  remove={this.deleteData}
-                />
-              }
-            />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    );
+              <Route
+                exact
+                path="/manajemen-buku"
+                element={
+                  <ManajemenBuku
+                    bookList={this.state.books}
+                    store={this.storeData}
+                    update={this.updateData}
+                    remove={this.deleteData}
+                  />
+                }
+              />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      );
+    }
   }
 }
